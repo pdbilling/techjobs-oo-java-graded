ce04aae393e7a5f775fa657b3d7f9d19d68ad0c4
Peter Billing <peterbilling@Peters-MacBook-Pro.local>
2022-10-01 16:41:08 -0500

    part 2 finished

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -6,6 +6,7 @@ public class CoreCompetency {
 
     private int id;
     private static int nextId = 1;
+
     private String value;
 
     public CoreCompetency() {
@@ -40,5 +41,18 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 
 }
